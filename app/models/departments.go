6e50bcbbd7ec7541e5a81ff4ec12ef9555f42f75
melvinjim <melvinjimenezang@hotmail.com>
2022-07-27 15:07:32 -0500

    Made some changes to previous migrations and created a new table along with the models.

--- app/models/departments.go
+++ app/models/departments.go
@@ -7,11 +7,9 @@ import (
 )
 
 type Departments struct {
-	ID          uuid.UUID `json:"id" db:"id"`
-	Name        string    `json:"name" db:"name"`
-	Description string    `json:"description" db:"description"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
-	RequirementType       *Requirement_type     `belongs_to:"requirement_types"`
+	ID              uuid.UUID         `json:"id" db:"id"`
+	Name            string            `json:"name" db:"name"`
+	Description     string            `json:"description" db:"description"`
+	CreatedAt       time.Time         `json:"created_at" db:"created_at"`
+	UpdatedAt       time.Time         `json:"updated_at" db:"updated_at"`
 }
-
