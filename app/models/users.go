015b1445a8f7e757ffdd7e5d0bab0801984b189c
MateoCaicedoW <mcaicedo@wawand.co>
2022-07-27 10:04:40 -0500

    adding migration and users models

---
+++ app/models/users.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type User struct {
+	ID           uuid.UUID `db:"id" `
+	Email        string    `db:"email" `
+	FirstName    string    `db:"first_name" `
+	LastName     string    `db:"last_name" `
+	DNI          string    `db:"dni" `
+	EmailAddress string    `db:"email_address" `
+	PhoneNumber  string    `db:"phone_number" `
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
+}
