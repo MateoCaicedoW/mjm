f2538bafe9e1958359b06a06227644e4ef41527a
melvinjim <melvinjimenezang@hotmail.com>
2022-07-27 10:33:24 -0500

    Created require_sub_types table models and migrations

---
+++ app/models/requirement.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type Requirements struct {
+	ID                uuid.UUID `json:"id" db:"id"`
+	RequirementTypeID uuid.UUID `json:"requirement_type_id" db:"requirement_type_id"`
+	Name              string    `json:"name" db:"name"`
+	CreatedAt         time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at" db:"updated_at"`
+}
