5b2ae863b4bbe10832468c065fac5d59d48f74a8
MateoCaicedoW <mcaicedo@wawand.co>
2022-07-27 11:09:50 -0500

    adding foreign key and many to one relation

--- app/models/departments.go
+++ app/models/departments.go
@@ -12,4 +12,5 @@ type Departments struct {
 	Description string    `json:"description" db:"description"`
 	CreatedAt   time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+	Users       *User     `belongs_to:"users"`
 }
