705c9cc1862c1ccea9e0819bc60083e0d86ecd50
MateoCaicedoW <mcaicedo@wawand.co>
2022-07-28 17:33:21 -0500

    task: adding requirement table

--- app/models/departments.go
+++ app/models/departments.go
@@ -12,6 +12,4 @@ type Departments struct {
 	Description string    `json:"description" db:"description"`
 	CreatedAt   time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
-	RequirementType       *Requirement_type     `belongs_to:"requirement_types"`
 }
-
