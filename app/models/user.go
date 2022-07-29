aa649293a0fa3cc0c62be45b24fc430fb88db39c
MateoCaicedoW <mcaicedo@wawand.co>
2022-07-29 11:53:36 -0500

    task: removing email duplicated in userModel

--- app/models/user.go
+++ app/models/user.go
@@ -8,7 +8,6 @@ import (
 
 type User struct {
 	ID           uuid.UUID    `db:"id" `
-	Email        string       `db:"email" `
 	FirstName    string       `db:"first_name" `
 	LastName     string       `db:"last_name" `
 	DNI          string       `db:"dni" `
