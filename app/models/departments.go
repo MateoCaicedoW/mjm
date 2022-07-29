802a59c5b7692a5ace0d31e43e3ccd4b751052fd
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-07-29 11:04:26 -0500

    adding select button when adding a new user

--- app/models/departments.go
+++ app/models/departments.go
@@ -12,5 +12,5 @@ type Departments struct {
 	Description     string            `json:"description" db:"description"`
 	CreatedAt       time.Time         `json:"created_at" db:"created_at"`
 	UpdatedAt       time.Time         `json:"updated_at" db:"updated_at"`
-	RequirementType *Requirement_type `belongs_to:"requirement_types"`
+	RequirementType *RequirementType `belongs_to:"requirement_types"`
 }
