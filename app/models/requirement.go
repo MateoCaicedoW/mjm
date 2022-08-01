ed42ca1c4e4213e8580f9d487a7768b9acec8366
MateoCaicedoW <mcaicedo@wawand.co>
2022-08-01 17:08:32 -0500

    adding null and foreign key in the user model

--- app/models/requirement.go
+++ app/models/requirement.go
@@ -3,37 +3,48 @@ package models
 import (
 	"time"
 
+	"github.com/gobuffalo/nulls"
 	"github.com/gofrs/uuid"
 )
 
 type Requirement struct {
-	ID                     uuid.UUID          `db:"id"`
-	Title                  string             `db:"title"`
-	Description            string             `db:"description"`
-	CreatedBy              uuid.UUID          `db:"created_by"`
-	CreatedAt              time.Time          `db:"created_at"`
-	RequestingDepartmentID uuid.UUID          `db:"requesting_department_id"`
-	ServiceDepartmentID    uuid.UUID          `db:"service_department_id"`
-	RequirementTypeID      uuid.UUID          `db:"requirement_type_id"`
-	RequirementSubTypeID   uuid.UUID          `db:"requirement_sub_type_id"`
-	ModifiedBy             uuid.UUID          `db:"modified_by"`
-	ModifiedAt             time.Time          `db:"modified_at"`
-	ApprovedBy             uuid.UUID          `db:"approved_by"`
-	ApprovedAt             time.Time          `db:"approved_at"`
-	AcceptedBy             uuid.UUID          `db:"accepted_by"`
-	AcceptedAt             time.Time          `db:"accepted_at"`
-	DeclinedBy             uuid.UUID          `db:"declined_by"`
-	DeclinedAt             time.Time          `db:"declined_at"`
-	ProcessedBy            uuid.UUID          `db:"proccessed_by"`
-	ProcessedAt            time.Time          `db:"proccessed_at"`
-	AssignedBy             uuid.UUID          `db:"assigned_by"`
-	AssignedTo             uuid.UUID          `db:"assigned_to"`
-	AssignedAt             time.Time          `db:"assigned_at"`
-	FinishedBy             uuid.UUID          `db:"finished_by"`
-	FinishedAt             time.Time          `db:"finished_at"`
-	Solved                 bool               `db:"solved"`
-	Department             Department         `has_one:"department"`
-	RequirementType        RequirementType    `has_one:"requirement_type"`
-	RequirementSubType     RequirementSubType `has_one:"requirement_sub_type"`
-	User                   User               `has_one:"user"`
+	ID                     uuid.UUID           `db:"id"`
+	Title                  string              `db:"title" fako:"job_title"`
+	Description            string              `db:"description" fako:"sentence"`
+	CreatedByUserID        uuid.UUID           `db:"created_by"`
+	CreatedAt              time.Time           `db:"created_at"`
+	RequestingDepartmentID uuid.UUID           `db:"requesting_department_id"`
+	ServiceDepartmentID    uuid.UUID           `db:"service_department_id"`
+	RequirementSubTypeID   uuid.UUID           `db:"requirement_sub_type_id"`
+	RequirementTypeID      uuid.UUID           `db:"requirement_type_id"`
+	ModifiedByUserID       nulls.UUID          `db:"modified_by"`
+	ModifiedAt             time.Time           `db:"modified_at"`
+	ApprovedByUserID       nulls.UUID          `db:"approved_by"`
+	ApprovedAt             time.Time           `db:"approved_at"`
+	AcceptedByUserID       nulls.UUID          `db:"accepted_by"`
+	AcceptedAt             time.Time           `db:"accepted_at"`
+	DeclinedByUserID       nulls.UUID          `db:"declined_by"`
+	DeclinedAt             time.Time           `db:"declined_at"`
+	ProcessedByUserID      nulls.UUID          `db:"processed_by"`
+	ProcessedAt            time.Time           `db:"processed_at"`
+	AssignedByUserID       nulls.UUID          `db:"assigned_by"`
+	AssignedToUserID       nulls.UUID          `db:"assigned_to"`
+	AssignedAt             time.Time           `db:"assigned_at"`
+	FinishedByUserID       nulls.UUID          `db:"finished_by"`
+	FinishedAt             time.Time           `db:"finished_at"`
+	UpdatedAt              time.Time           `db:"updated_at"`
+	Solved                 bool                `db:"solved"`
+	CreatedByUser          *User               `belongs_to:"users"`
+	RequestingDepartment   *Department         `belongs_to:"departments"`
+	ServiceDepartment      *Department         `belongs_to:"departments"`
+	RequirementSubType     *RequirementSubType `belongs_to:"requirement_sub_types"`
+	RequirementType        *RequirementType    `belongs_to:"requirement_types"`
+	ModifiedByUser         *User               `belongs_to:"users"`
+	ApprovedByUser         *User               `belongs_to:"users"`
+	AcceptedByUser         *User               `belongs_to:"users"`
+	DeclinedByUser         *User               `belongs_to:"users"`
+	ProcessedByUser        *User               `belongs_to:"users"`
+	AssignedToUser         *User               `belongs_to:"users"`
+	AssignedByUser         *User               `belongs_to:"users"`
+	FinishedByUser         *User               `belongs_to:"users"`
 }
