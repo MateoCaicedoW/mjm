90fcd4515383723e42ab344849d5dfa11423625a
wawandco <wawandco@MacBook-Pro-de-wawandco.local>
2022-08-02 17:24:54 -0500

    adding fako for model

--- app/models/requirement_type.go
+++ app/models/requirement_type.go
@@ -10,7 +10,7 @@ import (
 type RequirementType struct {
 	ID           uuid.UUID `form:"id" db:"id"`
 	DepartmentID uuid.UUID `form:"department_id" db:"department_id"`
-	Name         string    `form:"name" db:"name"`
+	Name         string    `form:"name" db:"name" fako:"domain_name"`
 	CreatedAt    time.Time `form:"created_at" db:"created_at"`
 	UpdatedAt    time.Time `form:"updated_at" db:"updated_at"`
 }
