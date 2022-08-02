26844057fe6c0e386bbfe537ab56579bdf3865dc
MateoCaicedoW <mcaicedo@wawand.co>
2022-08-02 08:47:14 -0500

    task: adding relations to table models

--- app/models/user.go
+++ app/models/user.go
@@ -7,14 +7,15 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID    `db:"id" `
-	FirstName    string       `db:"first_name" `
-	LastName     string       `db:"last_name" `
-	DNI          string       `db:"dni" `
-	EmailAddress string       `db:"email_address" `
-	PhoneNumber  string       `db:"phone_number" `
-	CreatedAt    time.Time    `db:"created_at"`
-	UpdatedAt    time.Time    `db:"updated_at"`
-	DepartmentID uuid.UUID    `db:"department_id"`
-	Departments  []Department `has_many:"deparments"`
+	ID           uuid.UUID     `db:"id" `
+	FirstName    string        `db:"first_name" `
+	LastName     string        `db:"last_name" `
+	DNI          string        `db:"dni" `
+	EmailAddress string        `db:"email_address" `
+	PhoneNumber  string        `db:"phone_number" `
+	CreatedAt    time.Time     `db:"created_at"`
+	UpdatedAt    time.Time     `db:"updated_at"`
+	DepartmentID uuid.UUID     `db:"department_id"`
+	Department   *Department   `belongs_to:"departments"`
+	Requirements []Requirement `has_many:"requirements"`
 }
