1cb151be5b9c4333de531c7c70524dc4a8c89530
wawandco <wawandco@MacBook-Pro-de-wawandco.local>
2022-08-02 17:19:31 -0500

    adding require rails

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -1,3 +1,4 @@
 require("expose-loader?exposes=$,jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require("@fortawesome/fontawesome-free/js/all.js");
+require("jquery-ujs/src/rails.js"); 
