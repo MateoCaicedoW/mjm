89a724018438ac3c86ff72b13975827b4f0d7aca
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-08-05 08:30:41 -0500

    rename and addig variable for routes

--- app/routes.go
+++ app/routes.go
@@ -6,7 +6,7 @@ import (
 	"mjm/app/actions/departments"
 	"mjm/app/actions/home"
 	requirement_type "mjm/app/actions/requeriment_type"
-	"mjm/app/actions/user"
+	"mjm/app/actions/users"
 
 	"mjm/app/middleware"
 	"mjm/public"
@@ -21,30 +21,32 @@ func setRoutes(root *buffalo.App) {
 	root.Use(middleware.CSRF)
 
 	root.GET("/", home.Index)
-	root.GET("/requirement-types", requirement_type.List)
-	root.GET("/requirement-type/new", requirement_type.New)
-	root.POST("/requirement-type/create", requirement_type.Create)
-	root.GET("/show/{requirement_type_id}", requirement_type.Show)
-	root.GET("/edit/{requirement_type_id}", requirement_type.Edit)
-	root.PUT("/update/{requirement_type_id}", requirement_type.Update)
-	root.DELETE("/delete/{requirement_type_id}", requirement_type.Delete)
-  
-	root.GET("/departments/list", departments.List)
-	root.GET("/department/new", departments.New)
-	root.POST("/department/create", departments.Create)
-	root.GET("/edit/{department_id}", departments.Edit)
-	root.GET("/show/{department_id}", departments.Show)
-	root.PUT("/update/{department_id}", departments.Update)
-	root.DELETE("/destroy/{department_id}", departments.Destroy)
+	requirementType := root.Group("/requirement-types")
+	requirementType.GET("/", requirement_type.List)
+	requirementType.GET("/new", requirement_type.New)
+	requirementType.POST("/create", requirement_type.Create)
+	requirementType.GET("/show/{requirement_type_id}", requirement_type.Show).Name("showRequirementTypes")
+	requirementType.GET("/edit/{requirement_type_id}", requirement_type.Edit).Name("editRequirementTypes")
+	requirementType.PUT("/update/{requirement_type_id}", requirement_type.Update).Name("updateRequirementTypes")
+	requirementType.DELETE("/delete/{requirement_type_id}", requirement_type.Delete).Name("deleteRequirementTypes")
 
-	root.GET("/users", user.List)
-	root.GET("/new-users", user.New)
-	root.POST("/create-user", user.Create)
-	root.GET("/edit-user/{user_id}", user.Edit)
-	root.PUT("/update-user/{user_id}", user.Update)
-	root.DELETE("/delete-user/{user_id}", user.Delete)
-	root.GET("/view-user/{user_id}", user.View)
+	department := root.Group("/departments")
+	department.GET("/", departments.List)
+	department.GET("/new", departments.New)
+	department.POST("/create", departments.Create)
+	department.GET("/edit/{department_id}", departments.Edit).Name("editDepartments")
+	department.GET("/show/{department_id}", departments.Show).Name("showDepartments")
+	department.PUT("/update/{department_id}", departments.Update).Name("updateDepartments")
+	department.DELETE("/destroy/{department_id}", departments.Destroy).Name("destroyDepartments")
 
+	user := root.Group("/users")
+	user.GET("/", users.List)
+	user.GET("/new", users.New)
+	user.POST("/create", users.Create)
+	user.GET("/edit/{user_id}", users.Edit).Name("editUsers")
+	user.PUT("/update/{user_id}", users.Update).Name("updateUsers")
+	user.DELETE("/delete/{user_id}", users.Delete).Name("deleteUsers")
+	user.GET("/view/{user_id}", users.View).Name("viewUsers")
 
 	root.ServeFiles("/", http.FS(public.FS()))
 }
