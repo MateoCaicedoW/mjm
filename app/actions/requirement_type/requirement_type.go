635b0476e1cfe47e193ea21cc2146c7693854ac1
wawandco <wawandco@MacBook-Pro-de-wawandco.local>
2022-08-16 15:45:07 -0500

    rebase

--- app/actions/requirement_type/requirement_type.go
+++ app/actions/requirement_type/requirement_type.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
+	"github.com/gofrs/uuid"
 
 	"github.com/gobuffalo/pop/v6"
 
@@ -62,16 +63,17 @@ func New(c buffalo.Context) error {
 
 func Create(c buffalo.Context) error {
 
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return fmt.Errorf("no transaction found")
+	}
 	requirementType := &models.RequirementType{}
 
 	if err := c.Bind(requirementType); err != nil {
 		return err
 	}
 
-	tx, ok := c.Value("tx").(*pop.Connection)
-	if !ok {
-		return fmt.Errorf("no transaction found")
-	}
+	requirementType.CreatedByUserID = uuid.Must(uuid.FromString("175afda1-82ef-4950-b8db-6dab15740d63"))
 
 	verrs, err := tx.ValidateAndCreate(requirementType)
 	if err != nil {
@@ -123,6 +125,8 @@ func Update(c buffalo.Context) error {
 	if err := c.Bind(requirementType); err != nil {
 		return err
 	}
+
+	 requirementType.CreatedByUserID = uuid.Must(uuid.FromString("175afda1-82ef-4950-b8db-6dab15740d63"))
 
 	verrs, err := tx.ValidateAndUpdate(requirementType)
 	if err != nil {
