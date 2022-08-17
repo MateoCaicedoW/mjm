a634645f510f95fa48216c02fc3bebb45dcb7d87
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-08-17 10:04:35 -0500

    adding ui users

--- app/models/user.go
+++ app/models/user.go
@@ -3,6 +3,9 @@ package models
 import (
 	"time"
 
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
 	"github.com/gofrs/uuid"
 )
 
@@ -19,6 +22,38 @@ type User struct {
 	DepartmentID uuid.UUID     `db:"department_id"`
 	Department   *Department   `belongs_to:"departments"`
 	Requirements []Requirement `has_many:"requirements"`
+}
+
+func (u *User) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{
+			Field:   u.FirstName,
+			Name:    "FirstName",
+			Message: "First name is required.",
+		},
+		&validators.StringIsPresent{
+			Field:   u.LastName,
+			Name:    "LastName",
+			Message: "Last name is required.",
+		},&validators.StringIsPresent{
+			Field:   u.DNI,
+			Name:    "DNI",
+			Message: "DNI is required.",
+		},&validators.StringIsPresent{
+			Field:   u.EmailAddress,
+			Name:    "EmailAddress",
+			Message: "Email address is required.",
+		},
+		&validators.StringIsPresent{
+			Field:   u.PhoneNumber,
+			Name:    "PhoneNumber",
+			Message: "Phone number is required.",
+		},
+
+		
+		
+	), nil
+
 }
 
 func (u Users) Map() map[string]uuid.UUID {
