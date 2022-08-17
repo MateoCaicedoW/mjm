5cf71dfb6ef3a242f23d03674bcd53461871a542
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-08-17 08:33:59 -0500

    change migratiom requirement type

--- migrations/20220816225716_add_records.postgres.up.sql
+++ migrations/20220816225716_add_records.postgres.up.sql
@@ -1,1 +1,1 @@
-INSERT INTO requirement_types values('9b66ccce-a2c9-4dbb-98d1-7f0abe641bc5', 'iOS', NOW(),NOW(),'Description', '7ae023e0-fc32-46de-a06c-a0c21f9286b2')
+INSERT INTO requirement_types values('9b66ccce-a2c9-4dbb-98d1-7f0abe641bc5', 'iOS', NOW(),NOW(),'Description', '175afda1-82ef-4950-b8db-6dab15740d63')
